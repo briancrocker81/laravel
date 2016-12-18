f0d9359c4eac44a53cf2dafe27d960a3583bdac1
Brian Crocker <briancrocker81@gmail.com>
2016-12-18 21:20:26 +0000

    added bower

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,6 @@
   <head>
     <title>Address book</title>
 
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
   </head>
 
   <body>
