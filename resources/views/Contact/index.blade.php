ba033fb077f548871a0e386b8ac7e14c0b1a4d53
Brian Crocker <briancrocker81@gmail.com>
2016-12-20 23:24:39 +0000

    tidy up of contact index table

--- resources/views/Contact/index.blade.php
+++ resources/views/Contact/index.blade.php
@@ -18,30 +18,40 @@
             <p>{{ $message }}</p>
         </div>
     @endif
-
-    <table class="table table-bordered">
-        <tr>
+    <div class="table-responsive">
+      <table class="table table-bordered table-striped table-hover">
+        <thead>
+          <tr>
             <th>No</th>
             <th>Title</th>
             <th>Name</th>
-            <th width="280px">Action</th>
-        </tr>
-    @foreach ($contacts as $key => $contact)
-    <tr>
-        <td>{{ ++$c }}</td>
-        <td>{{ $contact->title }}</td>
-        <td>{{ $contact->name }}</td>
-        <td>
-            <a class="btn btn-info" href="{{ route('contact.show',$contact->id) }}">Show</a>
-            <a class="btn btn-primary" href="{{ route('contact.edit',$contact->id) }}">Edit</a>
-            {!! Form::open(['method' => 'DELETE','route' => ['contact.destroy', $contact->id],'style'=>'display:inline']) !!}
-            {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-            {!! Form::close() !!}
-        </td>
-    </tr>
-    @endforeach
-    </table>
-
+            <th>Company Name</th>
+            <th>Email</th>
+            <th>Phone Number</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+        @foreach ($contacts as $key => $contact)
+          <tr>
+            <td>{{ ++$c }}</td>
+            <td>{{ $contact->title }}</td>
+            <td>{{ $contact->name }}</td>
+            <td>{{ $contact->company_name }}</td>
+            <td>{{ $contact->email }}</td>
+            <td>{{ $contact->phone_number }}</td>
+            <td>
+              <a class="btn btn-info" href="{{ route('contact.show',$contact->id) }}">Show</a>
+              <a class="btn btn-primary" href="{{ route('contact.edit',$contact->id) }}">Edit</a>
+              {!! Form::open(['method' => 'DELETE','route' => ['contact.destroy', $contact->id],'style'=>'display:inline']) !!}
+              {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+              {!! Form::close() !!}
+            </td>
+          </tr>
+        @endforeach
+        </tbody>
+      </table>
+    </div>
     {!! $contacts->render() !!}
 
 @endsection
